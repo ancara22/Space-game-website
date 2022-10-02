899a88fec71165c8c8cbd5c27779431a0acd7dce
Barcari Dionis <62211382+ancara22@users.noreply.github.com>
2022-10-02 17:30:02 +0100

    file aranging

--- src/php/menu.php
+++ src/php/menu.php
@@ -10,7 +10,7 @@ function renderMenu() {
 function renderNameForm() {
     echo "  
             <iframe name=\"noReload\" style=\"display:none;\"></iframe>
-            <form action=\"./src/php/menu.php\"  method=\"post\"  class=\"login_box\" target=\"noReload\">
+            <form action=\"./src/php/score.php\"  method=\"post\"  class=\"login_box\" target=\"noReload\">
                 <h2>Please enter your name:</h2>
                 <input id=\"name_input\" type=\"text\" name=\"player_name\" value=\"\">
                 <button class=\"btn btn_save\" id=\"btn_save_name\" type=\"submit\" name=\"submit\">Save</button>
@@ -18,29 +18,7 @@ function renderNameForm() {
 
 };
 
-function newPlayer($player_name) {
-    $playersData = fopen(__DIR__."/../dataBase/data_players.json", "a");
-    $txt = "\n".$player_name;
-    //fwrite($playersData, $txt);
 
-    $file = file_get_contents(__DIR__."/../dataBase/data_players.json");
-    $jsonf = json_decode($file, true);
-
-    $data = (object) array('name' => $player_name, 'score' => 0);
-
-    array_push($jsonf, $data);
-    $jsonData = json_encode($jsonf);
-    file_put_contents(__DIR__."/../dataBase/data_players.json", $jsonData);
-
-}
-
-
-
-$Name = isset($_POST['player_name']) ? $_POST['player_name'] : 'd';
-if(isset($_POST['player_name'])){
-    newPlayer($Name);
-
-}
 
 
 ?>
--- src/php/score.php
+++ src/php/score.php
@@ -1,14 +1,19 @@
 <?php
 
 
-function readScore() {
-    $file = file_get_contents("./src/dataBase/data_players.json");
+function newPlayer($player_name) {
+    $file = file_get_contents(__DIR__."/../dataBase/data_players.json");
     $jsonf = json_decode($file, true);
-
-    echo "<div style=\"z-index: 99999;\">".$jsonf["Denis"]["score"]."</div>";
-
-
+    $data = (object) array('name' => $player_name, 'score' => 0);
+    array_push($jsonf, $data);
+    $jsonData = json_encode($jsonf);
+    file_put_contents(__DIR__."/../dataBase/data_players.json", $jsonData);
 }
 
+$Name = isset($_POST['player_name']) ? $_POST['player_name'] : 'd';
+if(isset($_POST['player_name'])){
+    newPlayer($Name);
+
+}
 
 ?>
