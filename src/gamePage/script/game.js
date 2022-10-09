4f467ff10ed052a17b077ddf372aca78a49f2bb0
Denis <ancara22@gmail.com>
2022-10-09 01:59:38 +0100

    asteroids

--- src/gamePage/script/game.js
+++ src/gamePage/script/game.js
@@ -8,6 +8,7 @@ class CreateSpaceGame {
         this.cursorPosX = 0;
         this.cursorPosY = 0;
         this.shipSpeed = 20;
+        this.health = 100;
     }
 
     setCursorPositions(posX, posY) {
@@ -23,6 +24,13 @@ class CreateSpaceGame {
     }
 
     moveShip() {
+        if (this.shipSpeed == 3) {
+            setTimeout(() => {
+                this.shipSpeed = 20
+                document.getElementById("ship").style.opacity = "1"
+
+            }, 1000)
+        }
         let shipParam = ship.getBoundingClientRect(),
             backParam = gameworld.getBoundingClientRect();
 
@@ -59,6 +67,7 @@ class CreateSpaceGame {
                 gameworld.style.top = backParam.top + "px"
             }
         }
+
     }
 
     rotateShip(e) {
@@ -74,8 +83,132 @@ class CreateSpaceGame {
 
     }
 
+    createAsteroids(aster_nr) {
+        gameworld.innerHTML += "<div id=\"asteroids_box\"></div>";
+        let asteroids_box = document.getElementById("asteroids_box");
+
+        for (let i = 0; i < aster_nr; i++) {
+            let aster_posX = Math.round(Math.random() * 5000);
+            let aster_posY = Math.round(Math.random() * 5000);
+
+            asteroids_box.innerHTML += `<div style=\"top: ${aster_posY}px; left: ${aster_posX}px\" class=\"asteroid asteroid-${i}\"></div>`;
+
+        }
+
+    }
+
+
+
+    moveAsteroids() {
+        let asteroids = document.querySelectorAll(".asteroid"),
+            speed = 5;
+
+        asteroids.forEach(element => {
+            let asteroidSpeedX = Math.round(Math.random() * speed),
+                asteroidSpeedY = Math.round(Math.random() * speed),
+                directionX = Math.round(Math.random()) ? 1 : -1,
+                directionY = Math.round(Math.random()) ? 1 : -1,
+                background = Math.round((Math.random() * 4) + 1),
+                large_size = Math.round((Math.random() * 100)),
+                medium_size = Math.round((Math.random() * 100)),
+                asteroid_size;
+
+            let fromR = 0,
+                rotation_speed;
+            if (large_size > 70) {
+                asteroid_size = Math.round((Math.random() * 200) + 200)
+                rotation_speed = 0.5
+            } else if (medium_size > 50) {
+                rotation_speed = 1
+                asteroid_size = Math.round((Math.random() * 150) + 50)
+            } else {
+                rotation_speed = 3
+                asteroid_size = Math.round((Math.random() * 100) + 20)
+            }
+
+            element.style.width = asteroid_size + "px";
+            element.style.height = asteroid_size + "px";
+            element.style.backgroundImage = `url(\"../img/asteroid-0${background}.png\")`;
+
+
+            let windowX = parseInt(document.documentElement.clientWidth) / 2,
+                windowY = parseInt(document.documentElement.clientHeight) / 2,
+                viewportOffset, top, left, distance;
+
+            let explode = 1
+
+            setInterval(() => {
+
+                element.style.transform = `rotate(${fromR}deg)`;
+                fromR += rotation_speed;
+                element.style.left = parseInt(element.style.left) + asteroidSpeedX * directionX + "px";
+                element.style.top = parseInt(element.style.top) + asteroidSpeedY * directionY + "px";
+
+                viewportOffset = element.getBoundingClientRect();
+                top = viewportOffset.top;
+                left = viewportOffset.left;
+                distance = viewportOffset.width / 1.3
+
+
+                if (top <= windowY && top + distance >= windowY && left <= windowX && left + distance >= windowX) {
+                    if (explode == 1) {
+                        explode += 1;
+                        setTimeout(() => {
+                            this.shipSpeed = 3
+                            document.getElementById("ship").style.opacity = "0.3"
+                        }, 100)
+
+                        element.style.backgroundImage = "url(\"../img/expl-01.png\")";
+                        element.style.backgroundImage = "url(\"../img/expl-02.png\")";
+
+                        element.style.transform = "scale(1.7)";
+                        element.style.transition = "transform 1s ease 0s"
+                        element.style.transition = "background-image 0.4s ease 0s"
+                        element.style.transition = "opacity 3s ease 0s"
+
+                        element.style.opacity = "0";
+
+                    }
+
+
+                }
+
+
+
+                if (parseInt(element.style.left) < -400) {
+                    element.style.display = "none";
+                    element.style.left = 5000 + "px";
+                    setTimeout(() => element.style.display = "block", 1000)
+                } else if (parseInt(element.style.left) > 5400) {
+                    element.style.display = "none";
+                    element.style.left = 0 + "px";
+                    setTimeout(() => element.style.display = "block", 1000)
+                } else if (parseInt(element.style.top) > 5400) {
+                    element.style.display = "none";
+                    element.style.top = 0 + "px";
+                    setTimeout(() => element.style.display = "block", 1000)
+                } else if (parseInt(element.style.top) < -400) {
+                    element.style.display = "none";
+                    element.style.top = 5400 + "px";
+                    setTimeout(() => element.style.display = "block", 1000)
+                }
+            }, 100);
+
+
+
+        });
+
+
+
+
+
+    }
+
     startGame() {
         this.createWorldGrid(16);
+        this.createAsteroids(60);
+        this.moveAsteroids();
+
 
         document.addEventListener("mousemove", (evt) => {
             this.cursorPosX = evt.clientX;
@@ -88,10 +221,15 @@ class CreateSpaceGame {
         })
     }
 
+
+
 }
+
 
 
 
 
 let game = new CreateSpaceGame()
 game.startGame()
+
+
